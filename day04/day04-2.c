004cb9bdd67520357b9b9022100a1cd83815c317
HighVillain <hvkdw2458@gmail.com>
2024-09-23 14:57:18 +0900

    day04

---
+++ day04/day04-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap(int *num1, int *num2) {
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
+
+int main(void) {
+	
+	int a = 10;
+	int b = 20;
+
+	printf("a : %d b : %d\n", a, b);
+	swap(&a, &b);
+	printf("a : %d b : %d\n", a, b);
+
+	return 0;
+}
